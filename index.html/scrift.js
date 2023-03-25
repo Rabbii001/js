47053d5c496d7e5bc48f720c92c08c771f77a0eb
Rabbii001 <121807945+Rabbii001@users.noreply.github.com>
2023-03-25 14:04:38 +0600

    Add files via upload

---
+++ index.html/scrift.js
@@ -0,0 +1,7 @@
+<script>
+                var serchBox = document.getElementById("serchBox")
+                var googleIcon = document.getElementById("googleIcon")
+                googleIcon.onclick = function(){
+                serchBox.classList.toggle("active");
+                }
+            </script>
